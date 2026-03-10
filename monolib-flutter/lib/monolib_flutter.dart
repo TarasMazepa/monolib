95c93de9d5a265df05c20107d13b1015d79b1596
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2026-03-09 18:48:16 -0700

    Update monolib_flutter.dart

--- monolib-flutter/lib/monolib_flutter.dart
+++ monolib-flutter/lib/monolib_flutter.dart
@@ -1,6 +1,4 @@
 export 'src/flutter/on_build_context.dart';
 export 'src/flutter/on_scroll_controller.dart';
 export 'src/flutter/on_text_editing_value.dart';
-export 'src/ui_system/default_frame_size.dart';
-export 'src/ui_system/default_frame.dart';
 export 'src/ui_system/stream_builder_for_list.dart';
